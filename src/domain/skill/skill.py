f0844c3e608f427c57c174c04419a0d679ee3be3
yoshikyoto <stb_yoshiyuki-s@hotmail.co.jp>
2017-03-07 15:32:58 +0900

    domain skill

---
+++ src/domain/skill/skill.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+
+class Skill(object):
+
+    def __init__(self, key, name, icon_url):
+        self.__key = key
+        self.__name = name
+        self.__icon_url = icon_url
