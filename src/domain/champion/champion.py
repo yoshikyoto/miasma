e19ed6f3233cd99ae8bbb32c3d0beb162fce48a4
yoshikyoto <stb_yoshiyuki-s@hotmail.co.jp>
2017-03-07 16:14:38 +0900

    domain champion object

---
+++ src/domain/champion/champion.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+class Champion(object):
+
+    def __init__(self, id, key, name, icon_url, q, w, e, r):
+        self.__id = id
+        self.__key = key
+        self.__name = name
+        self.__icon_url = icon_url
+        self.__q = q
+        self.__w = w
+        self.__e = e
+        self.__r = r
+
+        
