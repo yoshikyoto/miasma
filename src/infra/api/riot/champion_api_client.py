93c0c5819887d39dfa28359c49175533947a8e73
yoshikyoto <stb_yoshiyuki-s@hotmail.co.jp>
2017-03-07 11:28:38 +0900

    config化

--- src/infra/api/riot/champion_api_client.py
+++ src/infra/api/riot/champion_api_client.py
@@ -1,14 +1,23 @@
 # -*- coding: utf-8 -*-
 import json
+import ConfigParser
 from riot_api_client import RiotApiClient
+
+import sys
+sys.path.append('../../../')
+from config import config
 
 class ChampionApiClient(RiotApiClient):
 
-    def __init__(self, api_key):
+    def __init__(self):
         super(ChampionApiClient, self).__init__()
         self.path = "/api/lol/jp/v1.2/champion"
-        self.api_key = api_key
+        self.api_key = config.RIOT_API_KEY
 
     def champions(self):
         path = self.path
         return super(ChampionApiClient, self).get(path)
+
+if __name__ == "__main__":
+    api = ChampionApiClient()
+    print api.champions()
