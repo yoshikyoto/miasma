d7ec0fa8bf751b5edb5cd8ba6faaf5ea6e913408
yoshikyoto <stb_yoshiyuki-s@hotmail.co.jp>
2017-03-07 23:45:05 +0900

    ChampionService

--- src/domain/champion/__init__.py
+++ src/domain/champion/__init__.py
@@ -1,2 +1,3 @@
 # -*- coding: utf-8 -*-
 from champion_repository import ChampionRepository
+from champion_service import ChampionService
---
+++ src/domain/champion/champion_service.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+import sys
+sys.path.append('../../')
+from domain.champion import ChampionRepository
+
+class ChampionService(object):
+
+    def __init__(self):
+        self.__repository = ChampionRepository()
+
+    def champions_dict(self):
+        champions = self.__repository.champions_from_cache()
+        result = []
+        for champion in champions:
+            result.append({
+                "name": champion.get_name(),
+                "icon_url": champion.get_icon_url(),
+            });
+        return result
+
+if __name__ == "__main__":
+    service = ChampionService()
+    print service.champions_dict()
