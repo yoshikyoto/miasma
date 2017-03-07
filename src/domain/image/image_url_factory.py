2bc99873d6be73dd0234502e2e4b13843094ddaf
yoshikyoto <stb_yoshiyuki-s@hotmail.co.jp>
2017-03-07 15:56:51 +0900

    image url factory でスキルの画像を表示できるように

--- src/domain/image/image_url_factory.py
+++ src/domain/image/image_url_factory.py
@@ -6,26 +6,34 @@ from infra.api.riot import RealmApiClient
 class ImageUrlFactory(object):
 
     def __init__(self):
-        self.realm_api = RealmApiClient()
-        self.realm = None
+        self.__realm_api = RealmApiClient()
+        self.__realm = None
 
     def __get_realm(self):
         """
         チャンピオンの画像を生成するたびにrealm apiを叩くのは効率が悪いので
         いい感じにキャッシュします
         """
-        if self.realm is None:
-            self.realm = self.realm_api.realm()
-        return self.realm
+        if self.__realm is None:
+            self.__realm = self.__realm_api.realm()
+        return self.__realm
 
     def champion_image_url(self, filename):
         realm = self.__get_realm()
         url = realm.get_cdn() + "/" + realm.get_champion() + "/img/champion/" + filename
         return url
 
+    def skill_image_url(self, filename):
+        realm = self.__get_realm()
+        url = realm.get_cdn() + "/" + realm.get_version() + "/img/spell/" + filename
+        return url
+
+
 if __name__ == "__main__":
-    factory = ImageUrlFacotry()
+    factory = ImageUrlFactory()
     print "do it"
     # 1回目はAPIアクセスがあるので時間がかかるけど2回目は早いはず
     print factory.champion_image_url("Ahri.png")
     print factory.champion_image_url("Ahri.png")
+    print factory.skill_image_url("AhriOrbofDeception.png")
+    print factory.skill_image_url("AhriOrbofDeception.png")
