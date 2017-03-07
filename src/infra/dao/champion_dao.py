6322b22a7fcb20a9d698432813ce08128014be38
yoshikyoto <stb_yoshiyuki-s@hotmail.co.jp>
2017-03-07 18:13:14 +0900

    dao

---
+++ src/infra/dao/champion_dao.py
@@ -0,0 +1,62 @@
+# -*- coding: utf-8 -*-
+from dao import Dao
+
+class ChampionDao(Dao):
+
+    def __init__(self):
+        super(ChampionDao, self).__init__()
+
+    def select_all(self):
+        connection = super(ChampionDao, self).connect()
+        cursor = super(ChampionDao, self).cursor(connection)
+        cursor.execute("SELECT * FROM `champion`")
+        result = cursor.fetchall()
+        cursor.close()
+        connection.close()
+        return result
+
+    def select(self, key):
+        connection = super(ChampionDao, self).connect()
+        cursor = super(ChampionDao, self).cursor(connection)
+        cursor.execute(
+            "SELECT * FROM `champion` WHERE `key` = %s",
+            [key])
+        result = cursor.fetchall()
+        cursor.close()
+        connection.close()
+        if(len(result) < 1):
+            return None
+        else:
+            return result[0]
+
+    def insert(self, id, key, name, icon_url):
+        connection = super(ChampionDao, self).connect()
+        cursor = super(ChampionDao, self).cursor(connection)
+        cursor.execute(
+            """
+            INSERT INTO `champion` (`id`, `key`, `name`, `icon_url`)
+            VALUES (%s, %s, %s, %s)
+            """,
+            [id, key, name, icon_url])
+        connection.commit()
+        cursor.close()
+        connection.close()
+
+    def delete(self, key):
+        connection = super(ChampionDao, self).connect()
+        cursor = super(ChampionDao, self).cursor(connection)
+        cursor.execute(
+            "DELETE FROM `champion` WHERE `key` = %s",
+            [key])
+        connection.commit()
+        cursor.close()
+        connection.close()
+
+if __name__ == "__main__":
+    dao = ChampionDao()
+    dao.insert(1, "test", "テスト", "url")
+    print dao.select("test")["name"]
+    print dao.select_all()
+    dao.delete("test")
+    print dao.select("test")
+    print dao.select_all()
---
+++ src/infra/dao/dao.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+import MySQLdb
+
+import sys
+sys.path.append('../../')
+from config import config
+
+class Dao(object):
+    
+    def __init__(self):
+        self.host = config.DB_HOST
+        self.db = config.DB_NAME
+        self.user = config.DB_USER
+        self.password = config.DB_PASSWORD
+
+    def connect(self):
+        return MySQLdb.connect(
+            host=self.host,
+            db=self.db,
+            user=self.user,
+            passwd=self.password)
+
+    def cursor(self, connection):
+        return connection.cursor(MySQLdb.cursors.DictCursor)
