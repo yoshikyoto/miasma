1048e0df114c7ce82acce8d1b579bc87097b0c3a
yoshikyoto <stb_yoshiyuki-s@hotmail.co.jp>
2017-03-07 20:02:18 +0900

    これでうごく

--- src/app.py
+++ src/app.py
@@ -8,5 +8,6 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-port = int(os.environ.get("PORT", 80))
-app.run(port=port)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(port=port)
