6b2cb654f8cfe88de96954e760cb22a681a25368
yoshikyoto <stb_yoshiyuki-s@hotmail.co.jp>
2017-04-04 11:24:24 +0900

    検索ができない

--- src/js/champions/champions.js
+++ src/js/champions/champions.js
@@ -1,60 +1,90 @@
 import React, {Component} from 'react'
 import {connect} from 'react-redux'
-import {GridList, GridTile} from 'material-ui/GridList';
+import {GridList, GridTile} from 'material-ui/GridList'
+import Subheader from 'material-ui/Subheader'
+import TextField from 'material-ui/TextField'
+import {Toolbar, ToolbarGroup, ToolbarTitle} from 'material-ui/Toolbar'
 import Axios from 'axios'
-import {renderChampions} from './reducer.js'
-import Champion from './champion.js'
+import {renderAllChampions, renderPartialChampions} from './reducer.js'
 
 /** チャンピオン一覧 */
 class Champions extends Component {
 
-
     render() {
         return (
             <div onload={this.getChampions()}>
+            
+            <Toolbar>
+            <ToolbarTitle text="チャンピオン一覧" />
+            <ToolbarGroup firstChild={true}>
+            <TextField
+            hintText="チャンピオンを検索"
+            onChange={this.searchChampions}
+            props={this.proos}/>
+            </ToolbarGroup>
+            </Toolbar>
+            
             <GridList
             cellHeight={120}
             cols={6}
             padding={10}
             style={{
-                width: 770, // 120 * 6 + 10 * 5 = 720 + 50
+                width: 790, // 120 * 6 + 10 * 7 = 720 + 70
                 marginLeft: 'auto',
                 marginRight: 'auto',
             }}>
+            
             {this.props.champions.map((champion) => {
                 return (
                     <GridTile
                     key={champion.key}
-                    title={champion.name}>
+                    title={champion.name}
+                    titleStyle={{
+                        fontSize: 12,
+                    }}>
                     <img src={champion.icon_url} />
                     </GridTile>
                 )
             })}
+            
             </GridList>
             </div>
         )
     }
 
-    /** チャンピオン一覧をまだ取得してなかったら取得して表示 */
+    /** チャンピオン一覧をまだ取得してなかったら取得してpropertyにset */
     getChampions() {
-        if(this.props.champions.length <= 1) {
+        if(this.props.allChampions.length <= 1) {
             Axios.get('/api/v1/champions.json').then(response => {
-                this.props.renderChampions(response.data.data);
+                this.props.renderAllChampions(response.data.data)
             });
         }
+    }
+
+    searchChampions(event, newValue) {
+        console.log("searchChampions: " + newValue);
+        console.log(this);
+        console.log(this.props);
+        console.log(event);
+            
+        props.renderPartialChampions([])
     }
 }
 
 function mapStateToProps(state) {
     return {
-        champions: state.championsReducer.champions
+        allChampions: state.championsReducer.allChampions,
+        champions: state.championsReducer.champions,
     }
 }
 
 function mapDispatchToProps(dispatch) {
     return {
-        renderChampions: (champions) => {
-            dispatch(renderChampions(champions))
+        renderAllChampions: (champions) => {
+            dispatch(renderAllChampions(champions))
+        },
+        renderPartialChampions: (champions) => {
+            dispatch(renderPartialChampions(champions))
         }
     }
 }
--- src/js/champions/reducer.js
+++ src/js/champions/reducer.js
@@ -1,21 +1,43 @@
 const initialState = {
-    champions: []
+    /** 全てのチャンピオン */
+    allChampions: [],
+    /** 画面に描画すべきチャンピオン */
+    champions: [],
 }
 
-const RENDER_CHAMPIONS = 'RENDER_CHAMPIONS'
+const RENDER_ALL_CHAMPIONS = 'RENDER_ALL_CHAMPIONS'
+const RENDER_PARTIAL_CHAMPIONS = 'RENDER_PARTIAL_CHAMPIONS'
 
-export function renderChampions(champions) {
+/** 全部のチャンピオンを描画する（初期化処理的な） */
+export function renderAllChampions(champions) {
     return {
-        type: RENDER_CHAMPIONS,
-        data: champions
+        type: RENDER_ALL_CHAMPIONS,
+        data: {
+            champions: champions,
+        }
+    }
+}
+
+/** 一部のチャンピオンのみを描画する */
+export function renderPartialChampions(champions) {
+    return {
+        type: RENDER_PARTIAL_CHAMPIONS,
+        data: {
+            champions: champions
+        }
     }
 }
 
 export default function reducer(state = initialState, action) {
     switch(action.type) {
-    case RENDER_CHAMPIONS:
+    case RENDER_ALL_CHAMPIONS:
         return Object.assign({}, state, {
-            champions: action.data
+            allChampions: action.data.champions,
+            champions: action.data.champions,
+        })
+    case RENDER_PARTIAL_CHAMPIONS:
+        return Object.assign({}, state, {
+            champions: action.data.champions
         })
     default:
         return state
