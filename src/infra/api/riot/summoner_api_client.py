25de933f38299e14ecf496459f691171cc8e533b
yoshikyoto <stb_yoshiyuki-s@hotmail.co.jp>
2017-04-06 19:19:45 +0900

    Summoner api client

---
+++ src/infra/api/riot/summoner_api_client.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+from riot_api_client import RiotApiClient
+
+class SummonerApiClient(RiotApiClient):
+
+    def __init__(self):
+        super(SummonerApiClient, self).__init__()
+        self.base_url = "https://jp.api.pvp.net"
+
+    def get_by_name(self, name):
+        path = "/api/lol/jp/v1.4/summoner/by-name/" + name
+        response = super(SummonerApiClient, self).get(path, {})
+        return response
+
+
+if __name__ == "__main__":
+    api = SummonerApiClient()
+    print api.get_by_name("うたかた")
