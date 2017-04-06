ef06c206e78e270ccb6d6ebda0bf72bf0aadba7e
yoshikyoto <stb_yoshiyuki-s@hotmail.co.jp>
2017-04-06 16:59:25 +0900

    new追加

--- src/config/news_config.py
+++ src/config/news_config.py
@@ -2,12 +2,17 @@
 
 news = [
     {
+        "date": "2017/04/06",
+        "text": "ScarzからKazu, Day1, Arford, DayDream の4人が脱退",
+        "link": "http://www.scarz.net/lol/1931",
+    },
+    {
         "date": "2017/04/05",
         "text": "チャンピオン紹介：ザヤ＆ラカン の情報が公開されました",
         "link": "http://jp.leagueoflegends.com/ja/featured/champion-xayah-rakan-reveal",
     },
     {
-        "data": "2017/04/05",
+        "date": "2017/04/05",
         "text": "パッチノート7.7 の情報が公開されました",
         "link": "http://jp.leagueoflegends.com/ja/news/game-updates/patch/patch-770-notes"
     }
