3690cf5fea3c0958dba82ed9fa429c07ec3db820
yoshikyoto <stb_yoshiyuki-s@hotmail.co.jp>
2017-04-07 13:39:50 +0900

    item api client

--- src/infra/api/riot/__init__.py
+++ src/infra/api/riot/__init__.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
 from champion_api_client import ChampionApiClient
 from summoner_api_client import SummonerApiClient
+from item_api_client import ItemApiClient
 from realm_api_client import RealmApiClient
---
+++ src/infra/api/riot/item_api_client.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+from riot_api_client import RiotApiClient
+
+class ItemApiClient(RiotApiClient):
+
+    def __init__(self):
+        super(ItemApiClient, self).__init__()
+        self.base_url = "https://global.api.pvp.net"
+        self.path = "/api/lol/static-data/jp/v1.2/item"
+
+    def items(self):
+        response = super(ItemApiClient, self).get(self.path, {})
+        return response
+
+if __name__ == "__main__":
+    api = ItemApiClient()
+    print api.items()
