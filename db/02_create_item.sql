6e23b46b9c380c108e59b1a6cbece411cbf53ae0
yoshikyoto <stb_yoshiyuki-s@hotmail.co.jp>
2017-04-07 16:35:20 +0900

    update item table

--- db/02_create_item.sql
+++ db/02_create_item.sql
@@ -1,7 +1,9 @@
+drop table item;
+
 create table item (
   id bigserial primary key,
   name varchar(255) not null,
-  description_html varchar(255),
+  description_html varchar(1024),
   icon_url varchar(255),
   created_at timestamp not null default current_timestamp
 );
--- src/domain/batch/update_table.py
+++ src/domain/batch/update_table.py
@@ -3,9 +3,21 @@ import sys
 sys.path.append('../../')
 sys.path.append('src') #heroku run での実行用
 from domain.champion import ChampionRepository
+from domain.item import ItemRepository
 
-repository = ChampionRepository()
-champions = repository.champions_from_api()
+print "update champions"
+champion_repository = ChampionRepository()
+champions = champion_repository.champions_from_api()
 for champion in champions:
-    repository.store_cache(champion)
-    print "update data " + champion.get_key() + " " + champion.get_name()
+    champion_repository.store_cache(champion)
+    print "update champion data " + champion.get_key() + " " + champion.get_name()
+
+print "update items"
+item_repository = ItemRepository()
+items = item_repository.items_from_api()
+# print items
+for item in items:
+    item_repository.store_cache(item)
+    print "update item data " + str(item.get_id()) + " " + item.get_name()
+
+print "end"
