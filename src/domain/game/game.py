90e2cd7e45f40f3d9ceaa9c88125d695aabaa377
yoshikyoto <stb_yoshiyuki-s@hotmail.co.jp>
2017-04-11 19:39:26 +0900

    gametype

--- src/domain/game/game.py
+++ src/domain/game/game.py
@@ -2,14 +2,43 @@
 
 class Game(object):
 
-    def __init__(self, id, team_id, champion, stats):
+    def __init__(self,
+                 id,
+                 game_type,
+                 game_mode,
+                 sub_type,
+                 team_id,
+                 champion,
+                 stats):
         self.__id = id
+        self.__game_type = game_type
+        self.__game_mode = game_mode
+        self.__sub_type = sub_type
         self.__team_id = team_id
         self.__champion = champion
         self.__stats = stats
 
     def get_id(self):
         return self.__id
+
+    def get_game_type(self):
+        return self.__game_type
+
+    def get_game_mode(self):
+        """
+        "CLASSIC": クラシック
+        """
+        return self.__game_mode
+
+    def get_sub_type(self):
+        """
+        "NORMAL": ノーマル
+        "RANKED_SOLO_5x5": ソロランク
+        """
+        return self.__sub_type
+
+    def is_ranked_solo(self):
+        return self.__sub_type == "RANKED_SOLO_5x5"
 
     def get_team_id(self):
         return self.__team_id
