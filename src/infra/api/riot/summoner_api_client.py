b2154e7ada1aceb8d54746428f56dc367f214714
yoshikyoto <stb_yoshiyuki-s@hotmail.co.jp>
2017-04-11 13:11:36 +0900

    summpner api client

--- src/infra/api/riot/summoner_api_client.py
+++ src/infra/api/riot/summoner_api_client.py
@@ -3,9 +3,9 @@ from riot_api_client import RiotApiClient
 
 class SummonerApiClient(RiotApiClient):
 
-    def __init__(self):
+    def __init__(self, region="jp"):
         super(SummonerApiClient, self).__init__()
-        self.base_url = "https://jp.api.pvp.net"
+        self.base_url = "https://" + region + ".api.pvp.net"
 
     def get_by_name(self, name):
         path = "/api/lol/jp/v1.4/summoner/by-name/" + name
@@ -15,3 +15,4 @@ class SummonerApiClient(RiotApiClient):
 if __name__ == "__main__":
     api = SummonerApiClient()
     print api.get_by_name("うたかた")
+    print api.get_by_name("destar")
