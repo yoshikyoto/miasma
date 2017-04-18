e2085a2501c0f6721cbfa2003db3be80ff963ff9
yoshikyoto <stb_yoshiyuki-s@hotmail.co.jp>
2017-04-18 22:45:00 +0900

    match api clinet into init

--- src/infra/api/riot/__init__.py
+++ src/infra/api/riot/__init__.py
@@ -6,3 +6,4 @@ from realm_api_client import RealmApiClient
 from game_api_client import GameApiClient
 from league_api_client import LeagueApiClient
 from stats_api_client import StatsApiClient
+from match_api_client import MatchApiClient
