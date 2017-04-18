adf361f317b605fd9b09f328217f9bddd8d1e231
yoshikyoto <stb_yoshiyuki-s@hotmail.co.jp>
2017-04-18 11:00:29 +0900

    player role

--- src/domain/game/__init__.py
+++ src/domain/game/__init__.py
@@ -1,2 +1,1 @@
 from game_repository import GameRepository
-
--- src/domain/game/stats.py
+++ src/domain/game/stats.py
@@ -18,3 +18,17 @@ class Stats(object):
         Player position (Legal values: TOP(1), MIDDLE(2), JUNGLE(3), BOT(4))
         """
         return self.__player_position
+
+    def get_player_role_str(self):
+        if self.__player_position == 1:
+            return "TOP"
+        elif self.__player_position == 2:
+            return "MID"
+        elif self.__player_position == 3:
+            return "JG"
+        else:
+            if self.__player_role == 2:
+                return "SUP"
+            else:
+                return "ADC"
+        
